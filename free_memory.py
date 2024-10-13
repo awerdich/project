ce582028a962141b3a5c1def664a0c3136bb078d
Andreas <andreas_werdich@hms.harvard.edu>
2024-10-13 10:52:08 -0400

    Added doc string to main function

--- free_memory.py
+++ free_memory.py
@@ -1,9 +1,8 @@
 #!/home/andreas/.pyenv/shims/python
 
 def main():
-	""" Return True if we have enough memory. False otherwise """"
+	""" Prints that everything is OK if there is enough free memory """"
 	print('Everything is OK.')
 
 if __name__ == '__main__':
 	main()
-	
