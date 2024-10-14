5306fe6d57c93c4d4b09927dffe94500cdbb1793
John Smith <john@crazyprogrammer.com>
2024-10-14 18:03:05 -0400

    create_file_list.py: creates a list of files in the working directory
    This script creates a list of .py files and outputs a .csv file with the name "file_list.csv".
    The function create_file_list() can be modified to search for other file types,
    such as text files.

---
+++ create_file_list.py
@@ -0,0 +1,20 @@
+#!/home/andreas/data/project_code/.venv/bin/python
+
+import os
+import glob
+import pandas as pd
+
+def create_file_list(extension='.py'):
+    """
+    Generates a DataFrame containing file names and their respective paths
+    for files in the current working directory.
+    """
+    file_list = glob.glob(os.path.join(os.getcwd(), f'*{extension}'), recursive=False)
+    file_path_list = [os.path.dirname(file) for file in file_list]
+    file_name_list = [os.path.basename(file) for file in file_list]
+    file_df = pd.DataFrame({'filename': file_name_list, 'path': file_path_list})
+    return file_df
+
+if __name__ == '__main__':
+    file_df = create_file_list()
+    file_df.to_csv('file_list.csv', index=False)
